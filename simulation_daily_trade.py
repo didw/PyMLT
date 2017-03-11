8cc69acdb0253e8db905650a28328221f30f517a
Jongyeol Yang <yangjy0113@gmail.com>
2017-03-11 23:01:44 +0900

    daily simulation is changed from keras to tensorflow

--- simulation_daily_trade.py
+++ simulation_daily_trade.py
@@ -2,96 +2,138 @@
 import pandas as pd
 import numpy as np
 import sqlite3
-from sklearn.ensemble.forest import RandomForestRegressor
+#from sklearn.ensemble.forest import RandomForestRegressor
 from sklearn.externals import joblib
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, normalization
-from keras.wrappers.scikit_learn import KerasRegressor
-from keras.models import model_from_json
-from keras import backend as K
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout, normalization
+#from keras.wrappers.scikit_learn import KerasRegressor
+#from keras.models import model_from_json
+#from keras import backend as K
 from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+import tensorflow as tf
 
 
-def load_model():
-    model_name = "../model/reg_keras/30_5_20110101_20160331.h5"
-    estimator = model_from_json(open(model_name.replace('h5', 'json')).read())
-    estimator.load_weights(model_name)
-    return estimator
+class Simulation:
+    def __init__(self):
+        self.len_past = 30
+        init_op = tf.global_variables_initializer()
+        self.config = tf.ConfigProto()
+        self.config.gpu_options.allow_growth = True
+        self.init_op = tf.global_variables_initializer()
+        self.s_date = "20120101_20160330"
+        self.model_dir = '../model/tf/regression/%s/' % self.s_date
 
+        # define variable
+        self.W1 = tf.Variable(tf.random_normal([690, 200], stddev=0.35), name="W1")
+        self.b1 = tf.Variable(tf.zeros([200]), name="b1")
+        self.W2 = tf.Variable(tf.random_normal([200, 1], stddev=0.35), name="W2")
+        self.b2 = tf.Variable(tf.zeros([1]), name="b2")
 
-def load_data(code, begin_date, end_date):
-    con = sqlite3.connect('../data/stock.db')
-    df = pd.read_sql("SELECT * from '%s'" % code, con, index_col='일자').sort_index()
-    data = df.loc[df.index > str(begin_date)]
-    data = data.loc[data.index < str(end_date)]
-    for col in data.columns:
+        # design graph
+        self.scalarInput =  tf.placeholder(shape=[None,690],dtype=tf.float32)
+        self.out1 = tf.matmul(self.scalarInput, self.W1) + self.b1
+        self.stream1 = tf.layers.dropout(tf.nn.relu(self.out1), rate=0.5)
+        #self.output = tf.layers.dense(self.stream1, 1)
+        self.output = tf.matmul(self.stream1, self.W2) + self.b2
+
+        self.saver = tf.train.Saver([self.W1, self.b1, self.W2, self.b2])
+
+    def load_scaler(self):
+        model_name = "../model/scaler_%s.pkl" % self.s_date
+        self.scaler = joblib.load(model_name)
+
+    def make_x(self, data, code):
+        data_x = []
+        days = []
+        for col in data.columns:
+            try:
+                data.loc[:, col] = data.loc[:, col].str.replace('--', '-')
+                data.loc[:, col] = data.loc[:, col].str.replace('+', '')
+            except AttributeError as e:
+                pass
+                print(e)
+        days = data.index[:]
         try:
-            data.loc[:, col] = data.loc[:, col].str.replace('--', '-')
-            data.loc[:, col] = data.loc[:, col].str.replace('+', '')
+            data.loc[:, 'month'] = data.index[:].str[4:6]
         except AttributeError as e:
-            pass
             print(e)
-    data = data.reset_index()
-    return data
+            print(data.index[:])
+        data = data.drop(['체결강도'], axis=1)
 
-def load_scaler(s_date):
-    model_name = "../model/scaler_%s.pkl" % s_date
-    return joblib.load(model_name)
+        # normalization
+        data = np.array(data)
+        if len(data) <= 0 :
+            return np.array([]), np.array([])
 
-def simulation_daily_trade(estimator, code, start_date, end_date):
-    code_data = load_data(code, start_date, end_date)
-    LEN_PAST = 30
-    MONEY = 100000
-    qty = 0
-    account_balance = 0
-    scaler = load_scaler("20110101_20160331")
-    print(len(code_data))
-    for idx in range(LEN_PAST, len(code_data)-1):
-        X_data = code_data[idx - LEN_PAST: idx]
-        X_data = X_data.reset_index()
-        cur_real_price = int(X_data.loc[29, '현재가'])
-        trade_price = int(code_data.loc[idx, '시가'])
-        X_data.loc[:, 'month'] = X_data.loc[:, '일자'].str[4:6]
-        X_data = X_data.drop(['index', '일자', '체결강도'], axis=1)
-        X_data = scaler[code[0]].transform(X_data)
-        cur_price = X_data[29][0]
-        X_data = np.array(X_data).reshape(-1, 30*23)
-        pred = estimator.predict(X_data)[0][0]
-        pred_transform = scaler[code[0]].inverse_transform([pred] + [0]*22)[0]
-        #print(pred, cur_price)
-        if pred_transform > 2*cur_real_price and qty == 0:
-            qty += (MONEY / cur_real_price + 1)
-            account_balance -= cur_real_price * (MONEY / cur_real_price + 1)
-            print("[BUY] balance: %d, price: %d qty: %d" % (account_balance, cur_real_price, qty))
-        if pred < cur_price and qty > 0:
+        self.load_scaler()
+        if code not in self.scaler:
+            print("code %s is not exist in scaler" % code)
+            return np.array([]), np.array([])
+        else:
+            data = self.scaler[code].transform(data)
+
+        for i in range(self.len_past, len(data)):
+            data_x.extend(np.array(data[i-self.len_past:i, :]))
+        np_x = np.array(data_x).reshape(-1, 23*30)
+        return np_x, days[self.len_past:]
+
+
+    def load_data(self, code, begin_date, end_date):
+        con = sqlite3.connect('../data/stock.db')
+        df = pd.read_sql("SELECT * from '%s'" % code, con, index_col='일자').sort_index()
+        data = df.loc[df.index > str(begin_date)]
+        data = data.loc[data.index < str(end_date)]
+        data_x, days = self.make_x(data, code)
+        assert len(data_x) == len(days)
+        return data_x, days
+
+    def predict(self, X_data):
+        with tf.Session(config=self.config) as sess:
+            sess.run(self.init_op)
+            ckpt = tf.train.get_checkpoint_state(self.model_dir)
+            self.saver.restore(sess, ckpt.model_checkpoint_path)
+            return sess.run(self.output, feed_dict={self.scalarInput: X_data})
+
+    def simulation_daily_trade(self, code, start_date, end_date):
+        X_data, days = self.load_data(code[0], start_date, end_date)
+        if len(X_data) == 0: return 0
+        MONEY = 100000
+        qty = 0
+        account_balance = 0
+        pred_list = self.predict(X_data)
+        for idx in range(len(X_data)):
+            pred = pred_list[idx][0]
+            cur_price = X_data[idx][29*23]
+            pred_transform = self.scaler[code[0]].inverse_transform([pred] + [0]*22)[0]
+            cur_real_price = self.scaler[code[0]].inverse_transform([cur_price] + [0]*22)[0]
+            #print(pred, cur_price)
+            if pred_transform > 2*cur_real_price and qty == 0:
+                qty += (MONEY / cur_real_price + 1)
+                account_balance -= cur_real_price * (MONEY / cur_real_price + 1)
+                print("[BUY] balance: %d, price: %d qty: %d" % (account_balance, cur_real_price, qty))
+            if pred < cur_price and qty > 0:
+                account_balance += 0.995 * cur_real_price * qty
+                qty = 0
+                print("[SELL] balance: %d, price: %d, qty: %d" % (account_balance, cur_real_price, qty))
+        if qty > 0:
             account_balance += 0.995 * cur_real_price * qty
-            qty = 0
-            print("[SELL] balance: %d, price: %d, qty: %d" % (account_balance, cur_real_price, qty))
-    if qty > 0:
-        account_balance += 0.995 * cur_real_price * qty
-        print("[L SELL] balance: %d, price: %d, qty: %d" % (account_balance, cur_real_price, qty))
-    return account_balance
+            print("[L SELL] balance: %d, price: %d, qty: %d" % (account_balance, cur_real_price, qty))
+        return account_balance
 
-def simulation_all_daily_trade(start_date, end_date):
-    estimator = load_model()
-    con = sqlite3.connect('../data/stock.db')
-    code_list = con.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
-    account_balance = 0
-    for code in code_list:
-        account_balance += simulation_daily_trade(estimator, code, start_date, end_date)
-        print("balance: %d" % account_balance)
-
-def set_config():
-    #Tensorflow GPU optimization
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
-    K.set_session(sess)
+    def simulation_all_daily_trade(self, start_date, end_date):
+        con = sqlite3.connect('../data/stock.db')
+        code_list = con.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+        account_balance = 0
+        idx = 0
+        for code in code_list:
+            account_balance += self.simulation_daily_trade(code, start_date, end_date)
+            idx += 1
+            print("[%d] balance: %d" % (idx, account_balance))
 
 
 if __name__ == '__main__':
-    set_config()
-    simulation_all_daily_trade('20160301', '20160501')
+    sm = Simulation()
+    sm.simulation_all_daily_trade('20160220', '20160501')
 
